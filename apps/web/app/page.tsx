9f207a1b3ed3f2ff7db391f250f71a5f7b0ec243
Cody Ogden <cody@codyogden.com>
2023-04-18 10:35:58 -0700

    Initial commit.

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from '../stories/Button';
+
+const IndexPage = () => {
+    return <>
+        <Button size='large' backgroundColor='pink' label='Click Me!' />
+    </>;
+};
+
+export default IndexPage;
